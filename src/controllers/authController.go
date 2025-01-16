1b2758f99c135ab23cdcf88a1b63a52a3104f593
Hamza Siddiqui <hamzapro285@gmail.com>
2025-01-16 17:25:57 +0500

    Token Setup

--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -1,11 +1,20 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/hamzapro305/GoJwt/src/services"
+)
 
 func Register(c *fiber.Ctx) error {
 	return c.SendString("Register")
 }
 
 func Login(c *fiber.Ctx) error {
-	return c.SendString("Login")
+	token, err := services.GetToken(c)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	return c.SendString(token)
 }
